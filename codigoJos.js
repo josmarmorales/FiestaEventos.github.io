c793b2b01f5da6bbdb7bc9f3b8d33280eea553dd
josmarmorales <161103056+josmarmorales@users.noreply.github.com>
2024-02-25 10:28:22 -0500

    initial commit

---
+++ codigoJos.js
@@ -0,0 +1,15 @@
+// Codigo de javaScrit
+
+
+//CONTACTANOS
+
+function confirmacion () {
+
+    var respuesta = confirm("¿Deseas enviar este formulario?");
+    if (respuesta == true){
+        return true;
+    } else {
+        return false;
+    } 
+}
+
